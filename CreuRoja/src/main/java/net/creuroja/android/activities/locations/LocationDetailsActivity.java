5149d06fa8b68d9743f1adc67222c51fa596a6bb
Denís Lapuente <lapuente@abbyy.com>
2014-11-10 17:39:40 +0100

    Add action bar for location details activity

--- CreuRoja/src/main/java/net/creuroja/android/activities/locations/LocationDetailsActivity.java
+++ CreuRoja/src/main/java/net/creuroja/android/activities/locations/LocationDetailsActivity.java
@@ -2,6 +2,7 @@ package net.creuroja.android.activities.locations;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -19,6 +20,9 @@ public class LocationDetailsActivity extends ActionBarActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_location_details);
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.location_detail_toolbar);
+		setSupportActionBar(toolbar);
 	}
 
 
