86138f2309ed768eba9ed59410ad5114aa0bb8b4
Denis Lapuente <darthsuicune@gmail.com>
2015-04-29 12:37:03 +0200

    Fix bug where login hints were displayed in white against a white background. Update to last gradle/support versions. Fix crash on update from Play store version.

--- CreuRoja/src/main/java/net/creuroja/android/model/db/CreuRojaOpenHelper.java
+++ CreuRoja/src/main/java/net/creuroja/android/model/db/CreuRojaOpenHelper.java
@@ -5,9 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class CreuRojaOpenHelper extends SQLiteOpenHelper {
-
 	private static final String DB_NAME = "CreuRoja";
-	private static final int DB_VERSION = 3;
+	private static final int DB_VERSION = 5;
 
 	private static final String CREATE = "CREATE TABLE ";
 	private static final String KEY = " INTEGER PRIMARY KEY AUTOINCREMENT, ";
@@ -23,26 +22,8 @@ public class CreuRojaOpenHelper extends SQLiteOpenHelper {
 		}
 		createLocationsTable(db);
 		createServicesTable(db);
-		createVehiclesTable(db);
 		createUsersTable(db);
 		createLocationServicesTable(db);
-	}
-
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		switch (oldVersion) {
-			case 1:
-				removeTable(db, CreuRojaContract.Users.TABLE_NAME);
-				createUsersTable(db);
-			case 2:
-				removeTable(db, CreuRojaContract.Services.TABLE_NAME);
-				removeTable(db, CreuRojaContract.Locations.TABLE_NAME);
-				createLocationsTable(db);
-				createServicesTable(db);
-				createLocationServicesTable(db);
-				break;
-		}
-
 	}
 
 	private void createLocationsTable(SQLiteDatabase db) {
@@ -81,21 +62,6 @@ public class CreuRojaOpenHelper extends SQLiteOpenHelper {
 				   CreuRojaContract.LocationServices.SERVICE_ID + " INTEGER NOT NULL)");
 	}
 
-	private void createVehiclesTable(SQLiteDatabase db) {
-		db.execSQL(CREATE + CreuRojaContract.Vehicles.TABLE_NAME + " (" +
-				   CreuRojaContract.Vehicles._ID + KEY +
-				   CreuRojaContract.Vehicles.BRAND + " TEXT NOT NULL, " +
-				   CreuRojaContract.Vehicles.MODEL + " TEXT NOT NULL, " +
-				   CreuRojaContract.Vehicles.LICENSE + " TEXT NOT NULL, " +
-				   CreuRojaContract.Vehicles.INDICATIVE + " TEXT NOT NULL, " +
-				   CreuRojaContract.Vehicles.VEHICLE_TYPE + " TEXT NOT NULL, " +
-				   CreuRojaContract.Vehicles.PLACES + " INTEGER NOT NULL, " +
-				   CreuRojaContract.Vehicles.NOTES + " TEXT NOT NULL, " +
-				   CreuRojaContract.Vehicles.OPERATIVE + " BOOLEAN NOT NULL, " +
-				   CreuRojaContract.Vehicles.CREATED_AT + " DATETIME NOT NULL, " +
-				   CreuRojaContract.Vehicles.UPDATED_AT + " DATETIME NOT NULL)");
-	}
-
 	private void createUsersTable(SQLiteDatabase db) {
 		db.execSQL(CREATE + CreuRojaContract.Users.TABLE_NAME + " (" +
 				   CreuRojaContract.Users._ID + KEY +
@@ -111,6 +77,20 @@ public class CreuRojaOpenHelper extends SQLiteOpenHelper {
 	}
 
 	private void removeTable(SQLiteDatabase db, String tableName) {
-		db.execSQL("DROP TABLE " + tableName);
+		db.execSQL("DROP TABLE IF EXISTS " + tableName);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		switch (oldVersion) {
+			default:
+				removeTable(db, CreuRojaContract.Users.TABLE_NAME);
+				removeTable(db, CreuRojaContract.Services.TABLE_NAME);
+				removeTable(db, CreuRojaContract.Locations.TABLE_NAME);
+				createUsersTable(db);
+				createLocationsTable(db);
+				createServicesTable(db);
+				createLocationServicesTable(db);
+		}
 	}
 }
